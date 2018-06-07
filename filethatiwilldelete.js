456f9ea1c1fe0b76a7677cd0cd838cb759ee21a0
Robin Diddams <robindiddams@gmail.com>
2018-06-07 09:07:08 -0700

    create files including the one i will delete

---
+++ filethatiwilldelete.js
@@ -0,0 +1,6 @@
+
+console.log("hello world")
+const doMath = () => {
+    return 1 + 1;
+};
+console.log(doMath)
---
+++ recognizableSourceCode/recognizableSourceCode.go
@@ -0,0 +1,15 @@
+package recognizableSourceCode
+
+import "fmt"
+
+func PrintNames() {
+	names := []string{
+		"jack",
+		"bob",
+		"john",
+		"elvis",
+		"piper",
+		"wren",
+	}
+	fmt.Println(names)
+}
