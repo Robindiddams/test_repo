deeb51a9aebe7da85139560ffd2ed6add2ecd6d2
Robin Diddams <robindiddams@gmail.com>
2018-06-08 12:10:47 -0700

    renamed util to utils and edited it a little

--- recognizableSourceCode/utils.go
+++ recognizableSourceCode/utils.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+// PrintArray will print an array of strings
 func PrintArray(things []string) {
 	for _, thing := range things {
 		fmt.Println(thing)
 	}
+	// also there is a comment here too
 }
